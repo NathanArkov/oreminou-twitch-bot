7f05f177b0294b56fa17816e3fbae802ced4f6d7
Nathan Arkov <90056309+NathanArkov@users.noreply.github.com>
2022-04-28 18:22:52 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const tmi = require('tmi.js');
+require('dotenv').config();
+const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
+
+const commands = {
+  don: {
+    response: 'Pour me faire un don (facultatif) : https://streamlabs.com/lytera_domino/tip'
+  },
+  insta: {
+    response: 'Mon Instagram : https://instagram.com/lytera_domino'
+  },
+  discord: {
+    response: 'Rejoins notre serveur Discord ici : https://discord.gg/H6ehavRz8U'
+  },
+  doudou: {
+    response: 'Je lance mon entreprise de doudous ! Ils sont fait à la main, cousus par moi même et fait avec beaucoup d\'amour. La première vente aura lieu le 29/04/2021 !'
+  },
+  youtube: {
+    response: 'Ma chaîne Youtube est encore en préparation ! Cette commande fonctionnera (avec le lien) lorsqu\'elle sera lancée !'
+  },
+  tdi: {
+    response: ''
+  }
+}
+
+const client = new tmi.Client({
+  connection: {
+    reconnect: true
+  },
+  channels: [
+    'lytera_domino'
+  ],
+  identity: {
+  username: process.env.TWITCH_BOT_USERNAME,
+  password: process.env.TWITCH_OAUTH_TOKEN
+}
+});
+
+client.connect();
+
+client.on('message', async (channel, context, message) => {
+  client.on('message', async (channel, context, message) => {
+    const isNotBot = context.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME.toLowerCase();
+
+    if ( !isNotBot ) return;
+
+    const [raw, command, argument] = message.match(regexpCommand);
+
+    const { response } = commands[command] || {};
+
+    if ( typeof response === 'function' ) {
+      client.say(channel, response(argument));
+    } else if ( typeof response === 'string' ) {
+      client.say(channel, response);
+    }
+});
+});
